5cea0a7058ca00cd4c96d0631338a1af5253a0e0
Cristhoper Mendez <51303481+Cristhoper-Mendez@users.noreply.github.com>
2025-06-04 21:44:07 -0600

    Se creo modal para agregar proveedor

--- resources/views/backend/proveedor/index.blade.php
+++ resources/views/backend/proveedor/index.blade.php
@@ -10,9 +10,9 @@
 @stop
 
 <style>
-    table{
+    table {
         /*Ajustar tablas*/
-        table-layout:fixed;
+        table-layout: fixed;
     }
 </style>
 
@@ -22,7 +22,8 @@
             <h1>Lista de Proveedores</h1>
         </div>
         <br>
-        <button type="button" style="font-weight: bold; background-color: #28a745; color: white !important;" onclick="" class="button button-3d button-rounded button-pill button-small">
+        <button type="button" style="font-weight: bold; background-color: #28a745; color: white !important;"
+            onclick="modalAgregar()" class="button button-3d button-rounded button-pill button-small">
             <i class="fas fa-pencil-alt"></i>
             Agregar Proveedor
         </button>
@@ -46,6 +47,65 @@
     </div>
 </section>
 
+<div class="modal fade" id="modalAgregar">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h4 class="modal-title">Nuevo Proveedor</h4>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <form id="formulario-nuevo">
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-md-12">
+
+                                <div class="form-group">
+                                    <label>Nombre</label>
+                                    <input type="text" maxlength="50" autocomplete="off" class="form-control"
+                                        id="nombre-nuevo" placeholder="Nombre">
+                                </div>
+
+                                <div class="form-group">
+                                    <label>Empresa</label>
+                                    <input type="text" maxlength="50" autocomplete="off" class="form-control"
+                                        id="empresa-nuevo" placeholder="Usuario">
+                                </div>
+
+                                <div class="form-group">
+                                    <label>Contacto</label>
+                                    <input type="text" maxlength="16" autocomplete="off" class="form-control"
+                                        id="contacto-nuevo" placeholder="Contraseña">
+                                </div>
+
+                                <div class="form-group">
+                                    <label style="color:#191818">Rol</label>
+                                    <br>
+                                    <div>
+                                        <select class="form-control" id="rol-nuevo">
+                                            <option value="null" selected disabled>Seleccione una opcion</option>
+                                            <option value="Mayorista">Mayorista</option>
+                                            <option value="Mayorista">Minotario</option>
+                                        </select>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                </form>
+            </div>
+            <div class="modal-footer justify-content-between">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
+                <button type="button" style="font-weight: bold; background-color: #28a745; color: white !important;"
+                    class="button button-3d button-rounded button-pill button-small"
+                    onclick="nuevoUsuario()">Guardar</button>
+            </div>
+        </div>
+    </div>
+</div>
 
 @extends('backend.menus.footerjs')
 @section('archivos-js')
@@ -61,10 +121,17 @@
 
     <!-- incluir tabla -->
     <script type="text/javascript">
-         $(document).ready(function(){
+        $(document).ready(function() {
             var ruta = "{{ URL::to('admin/proveedores/tabla') }}";
             $('#tablaDatatable').load(ruta);
             document.getElementById("divcontenedor").style.display = "block";
         });
+    </script>
+
+    <script>
+        function modalAgregar(){
+            document.getElementById("formulario-nuevo").reset();
+            $('#modalAgregar').modal('show');
+        }
     </script>
 @stop
